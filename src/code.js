c80783eb6bfcacd0f819f13aeafbe1fa93aedfba
Thomas Becker <tjbecker@me.com>
2019-09-04 04:42:17 +0300

    basic styling

--- src/code.js
+++ src/code.js
@@ -2,8 +2,29 @@ import React, {Component} from 'react';
 import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
 import CountriesCode from  './countriesCode';
-import './App.css';
+import styled from 'styled-components';
 
+const Wrapper = styled.section`
+  padding: 4em;
+  background: #f6f6f6;
+  border: 2px solid black;
+  border-radius; 3px;
+  text-align: center;
+  margin-top: 40px;
+  width: 15%;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Button =styled.button`
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 5px;
+  margin-top: 15px;
+  margin-left: auto;
+  margin-right: auto;
+`
 
 //apollo client setup
 const client = new ApolloClient({
@@ -17,10 +38,11 @@ class Code extends Component {
 render() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        
+      <Wrapper>
+      <div>
         <CountriesCode code={this.props.match.params.code} />
-        </div>
+      </div>
+      </Wrapper>
     </ApolloProvider>
   )
 }
